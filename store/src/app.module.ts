e3e5dd3ef52e7aa3e867c379ee6e14f1980aceb8
Lucas Mol <lucasmolpro@outlook.com>
2024-03-20 16:16:38 -0300

    feat: add new TypeOrmModule and ConfigModule (to use env vars on project) to main App Module

--- store/src/app.module.ts
+++ store/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.sb.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
